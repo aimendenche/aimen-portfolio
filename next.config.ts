4ec0ea2b2e9452d1c6d8376c7d6f23eda99cbbd8
aimen <denche@et.intechinfo.fr>
2025-01-20 09:53:50 +0100

    add rules

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // This disables ESLint checks during builds
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
